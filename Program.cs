75d9a9b120bb3f2cc6be553ab08c5d5f3ef16435
Dipps <Dipps@DESKTOP-IT87NAB>
2023-08-27 15:58:13 -1100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Channels;
+
+namespace Abstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SuperChef super = new SuperChef();
+            super.Country();
+
+        }
+
+
+    }
+
+
+      abstract class Chef
+      {
+        public abstract void Country();
+        
+          
+      }
+
+
+        class SuperChef : Chef
+    {
+
+        public override void Country()
+        {
+            Console.WriteLine(" this  is a super chef from Nigeria");
+        }
+
+    }
+
+
+
+
+
+}
